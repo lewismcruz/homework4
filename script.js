530470f86b248b1ed47a2c05670e35f6dfaaba79
lewismcruz <lewis.cruz@gmail.com>
2020-05-09 16:47:11 -0500

    Added psuedo code in script.js to outline game function and logic.

--- script.js
+++ script.js
@@ -1,2 +1,84 @@
 // Coding Game JavaScripts
 
+// Build a timed code quiz with multiple-choice questions.
+// App must run in the browser and feature dynamically updated HTML
+// and CSS powered by JavaScript code.
+
+// Must be clean and polished user interface and responsive.  Ensuring
+// it adapts to multiple screen sizes.
+
+// ***************** User Story **************
+// Timed quiz on JavaScript fundamentals that stores high scores
+// so I can gauge my progress compared to my peers
+
+
+
+// Click a start button
+// Use code from Password Generator homework assignment here to create button on page
+
+
+
+
+
+
+
+// Start a timer and present the first question
+
+
+     // While timer is ticking off need a condition to check for valid answers and for incorrect answers 
+     // need to interrupt while loop to else (wrong answer) condition and decrement timer value variable by
+     // 10000 milliseconds 
+
+
+
+
+// Answer the question then present another question
+
+
+
+
+// Create "Question Bank" of questions to present to user store them in an array that can be traversed randomly to
+// ask user a question.
+
+
+
+
+
+// Create answer array of correct answers and map it's index to the corresponding question it answers correctly.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// If question answered in correctly
+// Subtract time from the clock -- take off 10 seconds from timer
+
+// See While loop above
+
+
+
+
+
+// When all questions are answered or the timer reaches 0
+
+
+
+// The game is over
+
+
+// When the game is over allow user to save initials and score.
+
+var userInitials
+var userScore
+var topScore  // for comparison of scores
