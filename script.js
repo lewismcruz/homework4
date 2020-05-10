0e0a838915becd42a7a36cda8202ab7edf63bd5c
lewismcruz <lewis.cruz@gmail.com>
2020-05-10 11:37:25 -0500

    Created quiz and answer arrays.

--- script.js
+++ script.js
@@ -19,9 +19,6 @@
 
 
 
-
-
-
 // Start a timer and present the first question
 
 
@@ -30,7 +27,8 @@
      // 10000 milliseconds 
 
 
-
+     // Final condition to be met to stop the game is checking if all questions in the Question Array 
+     // were answered.
 
 // Answer the question then present another question
 
@@ -40,7 +38,9 @@
 // Create "Question Bank" of questions to present to user store them in an array that can be traversed randomly to
 // ask user a question.
 
-var arrQuestions = ["Question1", "Question2", "Question3", "Question4", "Question5", "Question6", "Question7", "Question8", "Question9", "Question10"];
+// Make arrQuestions an array of all questions asked
+
+var arrQuestions = ["Commonly used data types DO NOT include:", "The condition in an if / else statement is enclosed within_.", "Arrays in JavaScript can be used to store:", "String values must be enclosed within_____ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"];
 
 
 // Question 1 = Commonly used data types DO NOT include:
@@ -53,6 +53,26 @@ var arrQuestions = ["Question1", "Question2", "Question3", "Question4", "Questio
 // Question 4 = String values must be enclosed within_____ when being assigned to variables.
 
 // Question 5 = A very useful tool used during development and debugging for printing content to the debugger is:
+
+
+// Looping through Questions array
+for (var i = 0; i < arrQuestions.length; i++) {
+
+    if (arrQuestions[i] > 2){
+        //Question 1
+        userAnser = prompt("Commonly used data types DO NOT include:");
+
+    }
+    else {
+        incorrectUserAnswer = prompt("That is wrong.");
+    }
+
+}
+
+
+
+
+
 
 // *********************************
 // Final Screen is:
@@ -67,9 +87,12 @@ var arrQuestions = ["Question1", "Question2", "Question3", "Question4", "Questio
 
 // Create answer array of correct answers and map it's index to the corresponding question it answers correctly.
 
+// Use JavaScript object and property paring to then pair the correct answer to the corret question
+// and then map/link the correct answer to a button. All incorrect answers are mapped or links to "incorrect or wrong output message."
+
+var arrAnswers = ["strings", "booleans", "alerts", "numbers", "quotes", "curly brackets", "parenthesis", "square brackets", "numbers and strings", "other arrays", "booleans", "all of the above", "JavaScript", "terminal/bash", "for loops", "console log",];
 
 
-var arrAnswers = ["Answer1", "Answer2", "Answer3", "Answer4", "Answer5", "Answer6", "Answer7", "Answer8", "Answer9", "Answer10"];
 
 
 // Storage of user responses
@@ -77,15 +100,61 @@ var arrAnswers = ["Answer1", "Answer2", "Answer3", "Answer4", "Answer5", "Answer
 var userAnswer;
 
 
+// How to maybe relate the 2 arrays of arrAnswers and arrQuestions using JavaScript objects and properties.
+
+// Coupling of a property and a value:
+// Using a datatype
+// Use an object in JavaScript
 
 
 
+var arrQuestions = {
+    "Question1": [
+        "Choice1" = "This is choice 1.",
+        "Choice2" = "This is choice 2.",
+        "Choice3" = "This is choice 3.",
+        "Choice4" = "This is choice 4.",
+        "Choice5" = "This is choice 5.",
+    ],
+    "Question2": [
+        "Choice1" = "This is choice 1.",
+        "Choice2" = "This is choice 2.",
+        "Choice3" = "This is choice 3.",
+        "Choice4" = "This is choice 4.",
+        "Choice5" = "This is choice 5.",
+    ],
+    "Question3": [
+        "Choice1" = "This is choice 1.",
+        "Choice2" = "This is choice 2.",
+        "Choice3" = "This is choice 3.",
+        "Choice4" = "This is choice 4.",
+        "Choice5" = "This is choice 5.",
+    ],
+    "Question4": [
+        "Choice1" = "This is choice 1.",
+        "Choice2" = "This is choice 2.",
+        "Choice3" = "This is choice 3.",
+        "Choice4" = "This is choice 4.",
+        "Choice5" = "This is choice 5.",
+    ],
+    "Question5": [
+        "Choice1" = "This is choice 1.",
+        "Choice2" = "This is choice 2.",
+        "Choice3" = "This is choice 3.",
+        "Choice4" = "This is choice 4.",
+        "Choice5" = "This is choice 5.",
+    ],
+}
 
 
 
+// If question answered in correctly
+alert("Incorrect answer.  10 seconds off time deducted from the clock.")
 
 
-
+if (arrQuestions.Question1) {
+    var userQuestion1Answer = "Choice 1.";
+    var answerCorrect = arrQuestions[]
 // If question answered in correctly
 // Subtract time from the clock -- take off 10 seconds from timer
 
@@ -104,6 +173,17 @@ var userAnswer;
 
 // When the game is over allow user to save initials and score.
 
+
+
 var userInitials
 var userScore
 var topScore  // for comparison of scores
+
+
+// How is the final screen displayed?  Is it a new HTML page with All done! on it or
+// is it JavaScript?
+
+// Is Highscores page a new page?
+// Highscores simply displays the initials and the score.
+
+console.log(1.userInitials - userScore)   
