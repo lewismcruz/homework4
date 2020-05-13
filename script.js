854f242b4c253905b27593fa04b417bb8e65618d
lewismcruz <lewis.cruz@gmail.com>
2020-05-13 08:11:54 -0500

    Worked on page update using document.createElement on script.js

--- script.js
+++ script.js
@@ -25,20 +25,79 @@ var counter;
 var timeEl = document.querySelector(".time");
 var timeP = document.querySelector(".gameover");
 
+/*==========================================================================
+
+I tried to use this code example provided in class to change the questions
+and the choices to the next set of question and choices when a player selected
+the correct answer to the previous question.  
+
+My thought was to put this code in a advanceNextQuestion() function that could 
+be called with the player selected the correct answer for each question.
+
+
+// Set the body to a variable
 var body = document.body;
 
-
+// Create all necessary elements
+var h1El = document.createElement("h1");
 var h2El = document.createElement("h2");
+var infoEl = document.createElement("div");
+var imgEl = document.createElement("img");
+var cardBody = document.createElement("div");
+var nameEl = document.createElement("div");
+var favoriteEl = document.createElement("div");
+var listEl = document.createElement("ol");
 var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
 
+// Store our li elements in a variable
 var listItems = document.getElementsByTagName("li");
 
+// Set the text content of relevant elements
+h1El.textContent = "Welcome to my page";
+h2El.textContent = "This HTML document was created using JavaScript and Chrome Dev Tools";
+kittenEl.textContent = "This is my kitten";
+nameEl.textContent = "his name is Jax";
+favoriteEl.textContent = "My favorite foods are:";
+li1.textContent = "Chicken Fingers";
+li2.textContent = "Pizza";
+li3.textContent = "Burgers";
+li4.textContent = "Sushi";
+        
+    
+    // Append all of the new elements
+body.appendChild(h1El);
+body.appendChild(h2El);
+body.appendChild(infoEl);
+infoEl.appendChild(imgEl);
+infoEl.appendChild(kittenEl);
+infoEl.appendChild(nameEl);
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+    
+// Style all of our elements
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+h2El.setAttribute("style", "margin:auto; width:100%; text-align:center;");
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "http://placekitten.com/200/300");
+imgEl.setAttribute("height", 200);
+imgEl.setAttribute("width", 200);
+nameEl.setAttribute("style", "font-size:25px; text-align:center;");
+kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
+favoriteEl.setAttribute("style", "font-size:20px;");
+listEl.setAttribute("style", "background:#333333; padding:20px;");
+listItems[0].setAttribute("style", " color:blue; background: #666666; padding: 5px; margin-left: 35px;");
+listItems[1].setAttribute("style", " color:blue; background: #777777; padding: 5px; margin-left: 35px;");
+listItems[2].setAttribute("style", " color:orange; background: #888888; padding: 5px; margin-left: 35px;");
+listItems[3].setAttribute("style", " color:orange; background: #999999; padding: 5px; margin-left: 35px;");
 
-
-
+=========================================================================*/
 
 //var submitEl = document.querySelector("#submit");
 //var initialsInput = document.querySelector("#initials");
@@ -64,9 +123,6 @@ var correctResponse = "Correct" + choiceInput.
      // 10000 milliseconds 
 
 
-// Where does (document).ready go?
-// If you are using jQuery you need to link to jQuery library
-//$(document).ready(function() {
 
 // Need to stop the JavaScript from starting when the page refreshes.
 // Used 'event' inside setTime to then call the event.preventDefault method to keep the browser from refreshing.
@@ -154,7 +210,9 @@ var correctResponse = "Correct" + choiceInput.
         score++;
         alert("Correct");
         // Call function to load next set of questions to screen.
-        // function newQuestionLayout() {}
+        // function advanceNextQuestion() 
+        // to update page with next queston and choices once correct
+        // choice is selected/condition met.
         choiceSelected
         }
         else {
@@ -175,7 +233,14 @@ var correctResponse = "Correct" + choiceInput.
 
 
     
-     
+
+
+// function newQuestionLayout() {
+
+
+    // }
+    
+         
  
      
  
